8b02341f9bfb181a0cdd65318a26f2cdf97b1f32
Greg Jones <gjjones@outlook.com>
2025-12-20 06:53:35 -0500

    re-init to start from clean slate for new project

--- projects/tilttosketch/index.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  window.app.orientation = new app.engines.Orientation();
-  window.router = new app.router();
-  Backbone.history.start();
-  window.router.navigate(Backbone.history.fragment, {trigger: true});
-});
